00ba502d77b3b2c8aa370f6118cbd68419eda05a
Mejia, John {BIS} <john.mejia@pepsico.com>
2014-11-21 20:46:13 -0500

    commits

--- ClearStyle/ToDoItem.swift
+++ ClearStyle/ToDoItem.swift
@@ -9,5 +9,17 @@
 import UIKit
 
 class ToDoItem: NSObject {
+    
+    // A text description of this item.
+    var text: String
+    
+    // A Boolean value that determines the completed state of this item.
+    var completed: Bool
+    
+    // Returns a ToDoItem initialized with the given text and default completed value.
+    init(text: String) {
+        self.text = text
+        self.completed = false
+    }
    
 }
