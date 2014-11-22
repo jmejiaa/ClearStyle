2faf9767747dfbc78cdee3b491f411af0b6cf70a
Mejia, John {BIS} <john.mejia@pepsico.com>
2014-11-21 20:46:03 -0500

    Initial commit

---
+++ ClearStyle/ToDoItem.swift
@@ -0,0 +1,13 @@
+//
+//  ToDoItem.swift
+//  ClearStyle
+//
+//  Created by Mejia, John {BIS} on 11/21/14.
+//  Copyright (c) 2014 jmejiaa. All rights reserved.
+//
+
+import UIKit
+
+class ToDoItem: NSObject {
+   
+}
